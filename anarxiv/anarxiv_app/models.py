835990fc9bd6d56c930dede72376b40e83fc1acd
Jack Medley <jack.medley@ed.ac.uk>
2015-11-25 19:27:26 +0000

    Woo I've contributed.  Added User class with associated Django manager.  Made a few suggestions in Sam's 'Post' class too.  Login/Logout/Register/Forgotten Password views to follow

--- anarxiv/anarxiv_app/models.py
+++ anarxiv/anarxiv_app/models.py
@@ -1,20 +1,121 @@
 from django.db import models
-from django.utils import timezone
 import datetime
-
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 
 class Paper(models.Model):
-    author = models.CharField(max_length = 30)
-    title = models.CharField(max_length = 100)
-    abstract = models.TextField()
-    # journal_ref = models.CharField(max_length=200)
-    recordID = models.CharField(max_length = 100, default = '0')
-  
+
+	# Should these two be a TextField to prevert overflow?
+	author   = models.CharField(max_length=30)
+	title    = models.CharField(max_length=100)
+	abstract = models.TextField()
+	# journal_ref = models.CharField(max_length=200)
+	recordID = models.CharField(max_length=100, default='0')
 
 class Post(models.Model):
-	paperID = models.CharField(max_length = 100, default = '0')
-	message = models.TextField(default = "")
+
+	# Can a post have no message?  Surely we would reject that at the javascript level?
+	paperID = models.CharField(max_length=100, default='0')
+	message = models.TextField(default="")
+
+	# What does te first arg. here do?  Should this be done with Django's auto_now instead...
 	date = models.DateTimeField('date published', default=datetime.datetime.today)
+	# ...like this?
+	# created_at = models.DateTimeField(auto_now_add=True)
+	# updated_at = models.DateTimeField(auto_now=True)
 
+	# Posts should be one-to-one linked with a user
+	user  = models.OneToOneField(User)
 
+	# Posts should be many-to-one linked with a paper.  This lets you get all the post
+	# associated with a paper by doing stuff like...
+	#      paper.post_set.all()
+	# ...to get all posts for a paper or...
+	#      paper.post_set.filter(isAcademic=True)
+	# ...to get only the posts from acadmic users
+	paper = models.ForeignKey(Paper)
+
+	# Can each post be up-voted/down-voted?
+	upVotes = models.IntegerField(default=0)
+	dnVotes = models.IntegerField(default=0)
+
+	# Syntactic sugar methods
+	# What is the net 'scote'for this post?
+	@property
+	def votes(self): return self.upVotes - self.dnVotes
+
+	# Was this post by an academic?
+	@property
+	def isAcademic(self): return self.user.isAcademic
+
+	# What other methods should a post have?
+	def delete(self): pass
+	def reply (self): pass
+	def censor(self): pass
+
+# This defines a sort of handler class for the 'User' Django model below
+class AccountManager(BaseUserManager):
+
+	# This creates either a 'public' registered user with standard privalages or an 'academic' user
+	# with elevated privaleges.  Users are required to provide an email address, a username and a
+	# password (we can do things like password checking elsewhere).
+	def create_user(self, email, username, password, isAcademic=False):
+
+		# Make the model
+		account = self.model(email=self.normalize_email(email), username=username, isAcademic=isAcademic)
+
+		# DON'T save the password plaintext, instead let django hash+salt the password
+		account.set_password(password)
+
+ 		# Save the changes to the account
+		account.save()
+
+		# Return the instance
+		return account
+
+	# This creates 'proper' admins with top-level rights
+	def create_superuser(self, email, username, password, isAcademic=True):
+
+		# Create a regular user first
+		account = self.create_user(email, username, password, isAcademic)
+
+		# Then upgrade them to super user.
+		account.is_admin = True
+
+		# Save the changes to the account
+		account.save()
+
+		# Add all sites to the staff member
+		self.addAllSites(account)
+
+		# Return the instance
+		return account
+
+# This is the actual User Django model associated with registered users
+class User(AbstractBaseUser):
+
+	# Fields
+	username   = models.CharField(max_length=30)		# Equivalent to a screen name, used in correspondance
+	email      = models.EmailField(unique=True)		# Required to be unique, effectively our pk for Users
+	is_admin   = models.BooleanField(default=False)		# Superuser?
+	isAcademic = models.BooleanField(default=False)		# Academic privaleges?
+
+	# Record when account is created and modified
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	# Optionally expiry date if they are using a temporary password
+	passwordExpiry = models.DateTimeField(null=True)
+
+	# Overload objects using Django's internal user management
+	objects = AccountManager()
+
+	# Internal Django stuff
+	USERNAME_FIELD  = 'email'
+	REQUIRED_FIELDS = ['username']
+
+	@property
+	def is_superuser(self): return self.is_admin
+
+	# Provide a convenient output for 'print staffInstance'
+	def __unicode__(self): return self.email
